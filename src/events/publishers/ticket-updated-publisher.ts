fcbb300871f4f225bafcd4f4dc45f2991ae6786e
adc work <adc9work@outlook.com>
2020-09-17 15:42:47 +0530

    Ticketes

---
+++ src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from "@adc9tickets/common";
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
